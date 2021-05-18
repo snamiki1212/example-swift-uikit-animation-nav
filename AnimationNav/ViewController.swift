a6922adf02661e05c90731ede419f0ef3e4a2e6e
snamiki1212 <snamiki1212@gmail.com>
2021-05-17 21:29:49 -0700

    Add expand

--- AnimationNav/ViewController.swift
+++ AnimationNav/ViewController.swift
@@ -9,28 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var nav = UINavigationBar()
+    var navRightButton = UIButton()
+    
+    lazy var heightConstraint = nav.heightAnchor.constraint(equalToConstant: 88)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // nav
-        let nav = UINavigationBar(frame: CGRect(x: 0, y: 0, width: .max, height: 44))
+        nav = UINavigationBar(frame: CGRect(x: 0, y: 0, width: .max, height: 44))
         view.addSubview(nav)
         nav.backgroundColor = UIColor(red: 221, green: 221, blue: 221, alpha: 1)
         nav.translatesAutoresizingMaskIntoConstraints = false
         nav.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         nav.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         nav.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        nav.heightAnchor.constraint(equalToConstant: 88).isActive = true
-
+        heightConstraint.isActive = true
+        
         // nav right button constraints
-        let addButton = UIButton()
-        nav.addSubview(addButton)
-        addButton.setTitle("➕", for: .normal)
-        addButton.translatesAutoresizingMaskIntoConstraints = false
-        addButton.bottomAnchor.constraint(equalTo: nav.bottomAnchor).isActive = true
-        addButton.trailingAnchor.constraint(equalTo: nav.trailingAnchor).isActive = true
+        navRightButton = UIButton()
+        nav.addSubview(navRightButton)
+        navRightButton.setTitle("➕", for: .normal)
+        navRightButton.translatesAutoresizingMaskIntoConstraints = false
+        navRightButton.bottomAnchor.constraint(equalTo: nav.bottomAnchor).isActive = true
+        navRightButton.trailingAnchor.constraint(equalTo: nav.trailingAnchor).isActive = true
 
+        navRightButton.addTarget(self, action: #selector(onPressAddButton), for: .touchUpInside)
         
         view.backgroundColor = .red
         
@@ -40,6 +45,11 @@ class ViewController: UIViewController {
     
     @objc func onPressAddButton(){
         print("onPressAddButton")
+        
+        heightConstraint.constant = 200
+        UIView.animate(withDuration: 1) {
+            self.view.layoutIfNeeded()
+        }
     }
     
 
