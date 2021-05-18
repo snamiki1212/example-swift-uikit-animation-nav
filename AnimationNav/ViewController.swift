99ee488eb9e60d2aafd93a082d07c4b7c1d4973f
snamiki1212 <snamiki1212@gmail.com>
2021-05-17 17:45:24 -0700

    Initial Commit

---
+++ AnimationNav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationNav
+//
+//  Created by shunnamiki on 2021/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
